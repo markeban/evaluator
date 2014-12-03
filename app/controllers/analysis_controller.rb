535f2c69940d6772b2771319212fa5e415c834b9
Mark Richardson <markeban@yahoo.com>
2014-12-02 21:42:51 -0600

    added averages to charts

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -16,17 +16,25 @@ class AnalysisController < ApplicationController
     @teacher = Teacher.find_by(:id => params[:teacher_id])
     @recent_evaluation = @teacher.evaluations.last
     @submissions = @recent_evaluation.submissions
-    @submissions_avg = []
-    average = 0
-    @submissions.each do |submission|
-      submission.answers.each do |answer|
-        average += answer.answer.to_i
-      end
-      @submissions_avg << (average / submission.answers.length )
+    @questions_array = []
+    @averages_for_questions = []
+    @recent_evaluation.template.questions.each do |question|
+      @questions_array << question.text
+      answers = @recent_evaluation.answers.where(:question_id => question.id).map(&:answer).map(&:to_i)
+      @averages_for_questions << (answers.sum / answers.count.to_f).round(1)
     end
+    # @submissions.each_with_index do |submission, sub|
+    #   # submission.answers.each_with_index do |answer, ans|
+    #   #   puts @submissions_avg[ans]
+    #   #   @submissions_avg[ans] += answer.answer.to_i
+    #   # end
+    #   # @submissions_avg[sub] == (@submissions_avg[sub] / @submissions.length )
+    #   submission.
+    # end
+    puts "Stuff Below"
+    p @averages_for_questions
 
-
-    @questions_array = @recent_evaluation.template.questions.map(&:text)  
+    # @questions_array = @recent_evaluation.template.questions.map(&:text)  
 
   end
   
--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -2,6 +2,7 @@ class Evaluation < ActiveRecord::Base
   belongs_to :template
   belongs_to :teacher
   has_many :submissions
+  has_many :answers, :through => :submissions
 
 
   
