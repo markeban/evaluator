84a7ae65520fb254ea20fb6362aa3318b9b4848c
Mark Richardson <markeban@yahoo.com>
2015-01-02 07:11:11 -0600

    Moved logic from analysis controller to Model - evaluations.rb

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -1,5 +1,5 @@
 class AnalysisController < ApplicationController
-
+#Since there is no analysis.rb model, Model logic is in evaluation.rb
   def index
     @teacher_select = []   
     unique_teachers = current_user.teachers.uniq
@@ -23,82 +23,23 @@ class AnalysisController < ApplicationController
   
   def instructor_only_show
       @specific_evaluation = Evaluation.find_by(:id => params[:id])   
-      @submissions = @specific_evaluation.submissions
-      @teacher = @specific_evaluation.teacher
+      specific_scale_calculations = @specific_evaluation.scale_calculations
+      @submissions = specific_scale_calculations[0]
+      @teacher = specific_scale_calculations[1]
+      @questions_array_scale = specific_scale_calculations[2]
+      @averages_for_questions_scale = specific_scale_calculations[3]
+      @percentage_colors_scale = specific_scale_calculations[4]
 
-      @questions_array_scale = []
-      @averages_for_questions_scale = []
-      @specific_evaluation.template.questions.where(:format_type => "scale1To10").each do |question| 
-          @questions_array_scale << question.text
-          answers_scale = @specific_evaluation.answers.where(:question_id => question.id).map(&:answer).map(&:to_i)
-          @averages_for_questions_scale << (answers_scale.sum / answers_scale.count.to_f).round(1)
-      end
-      @percentage_colors_scale =[]
-      @averages_for_questions_scale.each do |average|
-        case 
-          when average > 9
-            @percentage_colors_scale << "#009900"
-          when average > 8
-            @percentage_colors_scale << "#39aa00"
-          when average > 7
-            @percentage_colors_scale << "#a3c400"
-          when average > 5
-            @percentage_colors_scale << "#d4b100"
-          when average > 3
-            @percentage_colors_scale << "#e67300"            
-          else
-            @percentage_colors_scale << "#ff0000"
-        end
-      end
+      specific_boolean_calculations = @specific_evaluation.boolean_calculations
+      @questions_array_boolean = specific_boolean_calculations[0]
+      @percentages_for_questions_boolean = specific_boolean_calculations[1]
+      @percentage_colors_boolean = specific_boolean_calculations[2]
 
-      @questions_array_boolean = []
-      @percentages_for_questions_boolean = []
-      @specific_evaluation.template.questions.where(:format_type => "boolean").each do |question|
-        @questions_array_boolean << question.text
-        answers_boolean = @specific_evaluation.answers.where(:question_id => question.id).map(&:answer).map(&:to_i)
-        @percentages_for_questions_boolean << (answers_boolean.sum / answers_boolean.count.to_f).round(2)
-      end
+      specific_multiple_choice_calculations = @specific_evaluation.multiple_choice_calculations
+      @questions_array_multiple_choice = specific_multiple_choice_calculations[0]
+      @percentages_for_questions_multiple_choice = specific_multiple_choice_calculations[1]
 
-      @percentage_colors_boolean =[]
-      @percentages_for_questions_boolean.each do |percentage|
-        case 
-          when percentage > 0.9
-            @percentage_colors_boolean << "#009900"
-          when percentage > 0.8
-            @percentage_colors_boolean << "#39aa00"
-          when percentage > 0.7
-            @percentage_colors_boolean << "#a3c400"
-          when percentage > 0.5
-            @percentage_colors_boolean << "#d4b100"
-          when percentage > 0.3
-            @percentage_colors_boolean << "#e67300"            
-          else
-            @percentage_colors_boolean << "#ff0000"
-        end
-      end
-
-      @questions_array_multiple_choice = []
-      @percentages_for_questions_multiple_choice = []
-      @specific_evaluation.template.questions.where(:format_type => "multipleChoice").each do |question| 
-        @questions_array_multiple_choice << question.text
-        answers_multiple_choice = @specific_evaluation.answers.where(:question_id => question.id).map(&:answer)
-        question.question_options.each do |option|
-          @percentages_for_questions_multiple_choice << [option.text, (answers_multiple_choice.count(option.text).to_f / answers_multiple_choice.length)
-            .round(2)]
-        end
-      end
-
-      questions_text =[]
-      answers_text =[]
-      @specific_evaluation.template.questions.where(:format_type => "text").each do |question| 
-        questions_text << question.text
-        answers_text << @specific_evaluation.answers.where(:question_id => question.id).map(&:answer)
-      end
-      @questions_answers = []
-      questions_text.each_with_index do |question, index|
-        @questions_answers << [question, answers_text[index]]
-      end
-
+      @questions_answers = @specific_evaluation.text_calculations
   end
 
 
@@ -107,48 +48,11 @@ class AnalysisController < ApplicationController
     @teacher = Teacher.find_by(:id => params[:teacher])
     @evaluations = @specific_template.evaluations.where(:teacher_id => @teacher.id)
 
-    averages_of_all_questions_array_scale = []
-    @evaluation_start_dates = []
+    specific_instructor_only_scale_calculations = @evaluations.instructor_only_scale_calculations
+    @evaluation_start_dates = specific_instructor_only_scale_calculations[0]
+    @averages = specific_instructor_only_scale_calculations[1]
 
-    questions_scale = @evaluations.first.submissions.first.questions.where(:format_type => "scale1To10").map(&:text)
-    @evaluations.each do |evaluation|
-      @evaluation_start_dates << evaluation.created_at.strftime("%A, %b, %d %Y %l:%M %p")
-      averages_for_questions_scale = []
-      evaluation.template.questions.where(:format_type => "scale1To10").each do |question| 
-          answers_scale = evaluation.answers.where(:question_id => question.id).map(&:answer).map(&:to_i)
-          averages_for_questions_scale << (answers_scale.sum / answers_scale.count.to_f).round(1)
-      end
-      averages_of_all_questions_array_scale << averages_for_questions_scale  
-    end
-    averages_of_all_questions_array_scale_trans = averages_of_all_questions_array_scale.transpose
-    @averages = {}
-    questions_scale.each_with_index do |question, index|
-      @averages[question] = averages_of_all_questions_array_scale_trans[index]
-    end
-
-
-    questions_boolean = @evaluations.last.submissions.first.questions.where(:format_type => "boolean").map(&:text)
-    averages_of_all_questions_array_boolean = []
-    @evaluations.each do |evaluation|
-      questions_array_boolean = []
-      averages_for_questions_boolean = []
-      evaluation.template.questions.where(:format_type => "boolean").each do |question|
-        questions_array_boolean << question.text
-        answers_boolean = evaluation.answers.where(:question_id => question.id).map(&:answer).map(&:to_i)
-        averages_for_questions_boolean << (answers_boolean.sum / answers_boolean.count.to_f).round(2)
-      end
-      averages_of_all_questions_array_boolean << averages_for_questions_boolean
-     
-
-    end
-    averages_of_all_questions_array_boolean_trans = averages_of_all_questions_array_boolean.transpose
-    @averages_boolean = {}
-    questions_boolean.each_with_index do |question, index|
-      @averages_boolean[question] = averages_of_all_questions_array_boolean_trans[index]
-    end
-
-
-
+    @averages_boolean = @evaluations.instructor_only_boolean_calculations
   end
 
 
--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -5,5 +5,136 @@ class Evaluation < ActiveRecord::Base
   has_many :answers, :through => :submissions
 
 
-  
+  def scale_calculations
+    @submissions = self.submissions
+    @teacher = self.teacher
+
+        @questions_array_scale = []
+        @averages_for_questions_scale = []
+        self.template.questions.where(:format_type => "scale1To10").each do |question| 
+            @questions_array_scale << question.text
+            answers_scale = self.answers.where(:question_id => question.id).map(&:answer).map(&:to_i)
+            @averages_for_questions_scale << (answers_scale.sum / answers_scale.count.to_f).round(1)
+        end
+        @percentage_colors_scale =[]
+        @averages_for_questions_scale.each do |average|
+          case 
+            when average > 9
+              @percentage_colors_scale << "#009900"
+            when average > 8
+              @percentage_colors_scale << "#39aa00"
+            when average > 7
+              @percentage_colors_scale << "#a3c400"
+            when average > 5
+              @percentage_colors_scale << "#d4b100"
+            when average > 3
+              @percentage_colors_scale << "#e67300"            
+            else
+              @percentage_colors_scale << "#ff0000"
+          end
+        end
+    return [@submissions, @teacher, @questions_array_scale, @averages_for_questions_scale, @percentage_colors_scale]  
+  end
+
+  def boolean_calculations
+    @questions_array_boolean = []
+    @percentages_for_questions_boolean = []
+      self.template.questions.where(:format_type => "boolean").each do |question|
+        @questions_array_boolean << question.text
+        answers_boolean = self.answers.where(:question_id => question.id).map(&:answer).map(&:to_i)
+        @percentages_for_questions_boolean << (answers_boolean.sum / answers_boolean.count.to_f).round(2)
+      end
+
+      @percentage_colors_boolean =[]
+      @percentages_for_questions_boolean.each do |percentage|
+        case 
+          when percentage > 0.9
+            @percentage_colors_boolean << "#009900"
+          when percentage > 0.8
+            @percentage_colors_boolean << "#39aa00"
+          when percentage > 0.7
+            @percentage_colors_boolean << "#a3c400"
+          when percentage > 0.5
+            @percentage_colors_boolean << "#d4b100"
+          when percentage > 0.3
+            @percentage_colors_boolean << "#e67300"            
+          else
+            @percentage_colors_boolean << "#ff0000"
+        end
+      end
+    return [@questions_array_boolean, @percentages_for_questions_boolean, @percentage_colors_boolean]  
+  end
+
+  def multiple_choice_calculations
+    @questions_array_multiple_choice = []
+      @percentages_for_questions_multiple_choice = []
+      self.template.questions.where(:format_type => "multipleChoice").each do |question| 
+        @questions_array_multiple_choice << question.text
+        answers_multiple_choice = self.answers.where(:question_id => question.id).map(&:answer)
+        question.question_options.each do |option|
+          @percentages_for_questions_multiple_choice << [option.text, (answers_multiple_choice.count(option.text).to_f / answers_multiple_choice.length)
+            .round(2)]
+        end
+      end
+    return [@questions_array_multiple_choice, @percentages_for_questions_multiple_choice]
+  end
+
+  def text_calculations
+    questions_text = []
+    answers_text = []
+      self.template.questions.where(:format_type => "text").each do |question| 
+        questions_text << question.text
+        answers_text << self.answers.where(:question_id => question.id).map(&:answer)
+      end
+      @questions_answers = []
+      questions_text.each_with_index do |question, index|
+        @questions_answers << [question, answers_text[index]]
+      end
+    return @questions_answers
+  end
+
+  def instructor_only_scale_calculations
+    averages_of_all_questions_array_scale = []
+    @evaluation_start_dates = []
+
+    questions_scale = self.first.submissions.first.questions.where(:format_type => "scale1To10").map(&:text)
+    self.each do |evaluation|
+      @evaluation_start_dates << evaluation.created_at.strftime("%A, %b, %d %Y %l:%M %p")
+      averages_for_questions_scale = []
+      evaluation.template.questions.where(:format_type => "scale1To10").each do |question| 
+          answers_scale = evaluation.answers.where(:question_id => question.id).map(&:answer).map(&:to_i)
+          averages_for_questions_scale << (answers_scale.sum / answers_scale.count.to_f).round(1)
+      end
+      averages_of_all_questions_array_scale << averages_for_questions_scale  
+    end
+    averages_of_all_questions_array_scale_trans = averages_of_all_questions_array_scale.transpose
+    @averages = {}
+    questions_scale.each_with_index do |question, index|
+      @averages[question] = averages_of_all_questions_array_scale_trans[index]
+    end
+    return [@evaluation_start_dates, @averages]
+  end
+
+  def instructor_only_boolean_calculations
+    questions_boolean = self.last.submissions.first.questions.where(:format_type => "boolean").map(&:text)
+    averages_of_all_questions_array_boolean = []
+    self.each do |evaluation|
+      questions_array_boolean = []
+      averages_for_questions_boolean = []
+      evaluation.template.questions.where(:format_type => "boolean").each do |question|
+        questions_array_boolean << question.text
+        answers_boolean = evaluation.answers.where(:question_id => question.id).map(&:answer).map(&:to_i)
+        averages_for_questions_boolean << (answers_boolean.sum / answers_boolean.count.to_f).round(2)
+      end
+      averages_of_all_questions_array_boolean << averages_for_questions_boolean
+     
+
+    end
+    averages_of_all_questions_array_boolean_trans = averages_of_all_questions_array_boolean.transpose
+    @averages_boolean = {}
+    questions_boolean.each_with_index do |question, index|
+      @averages_boolean[question] = averages_of_all_questions_array_boolean_trans[index]
+    end
+    return @averages_boolean
+  end
 end
