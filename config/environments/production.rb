df6b240f15f829a4e31a04c0efdfa2e0ffccb5c3
Mark Richardson <mark@markeban.com>
2016-09-26 17:17:25 -0700

    add logger to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,8 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
+  RAILS_DEFAULT_LOGGER = Logger.new('log/production.log')
+  
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,10 +12,7 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
   
-  config.secret_key = ENV['DEVISE_SECRET_KEY'] if Rails.env == 'production'
-  puts "coolness"
-  p ENV['DEVISE_SECRET_KEY']
-
+  
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
