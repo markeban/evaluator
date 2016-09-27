7fad7cdcec72ed000313b1a96b84639d79bef347
Mark Richardson <mark@markeban.com>
2016-09-26 17:06:21 -0700

    devise secret key production

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
   
-  config.secret_key = ENV['DEVISE_SECRET_KEY']
+  config.secret_key = ENV['DEVISE_SECRET_KEY'] if Rails.env == 'production'
 
 
   # Configure the class responsible to send e-mails.
